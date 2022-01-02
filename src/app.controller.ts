6fc95c4c24a6a9cf7ddc6836652f1ce0faaa0178
AntoineDrsl <antoine.durussel@gmail.com>
2022-01-02 17:33:50 +0100

    :sparkles: FEAT launch script

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { execSync } from 'child_process';
+import {v4 as uuidv4} from 'uuid';
+import * as fs from 'fs';
 
 @Controller()
 export class AppController {
@@ -8,5 +11,49 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('editor')
+  launchScript(@Body() body) 
+  {
+    let extension = '';
+    let executable = ''; 
+    switch(body.language) {
+      case 'php':
+        extension = 'php'
+        executable = 'php'
+        break
+
+      case 'javascript':
+        extension = 'js'
+        executable = 'node'
+        break
+
+      case 'python':
+        extension = 'py'
+        executable = 'python3'
+        break
+
+      default:
+        return 'Invalid language'
+    }
+
+    let fileName: string = uuidv4() + '.' + extension;
+    let filePath: string = 'uploads/temp/' + fileName;
+    fs.appendFileSync(filePath, body.code);
+
+    let result = {};
+    try {
+      result = {
+        result: execSync(`${executable} ${filePath} ?>&1`).toString()
+      };
+    } catch(err) {
+      result = {
+        error: err.stderr.toString()
+      }
+    }
+    fs.unlinkSync(filePath);
+
+    return result;
   }
 }
