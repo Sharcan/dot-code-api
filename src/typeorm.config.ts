69e9ede6b386a73261d71fedd2dcdd380fe38ec9
AntoineDrsl <antoine.durussel@gmail.com>
2022-03-18 10:57:38 +0100

    :bug: FIX docker typeorm

---
+++ src/typeorm.config.ts
@@ -0,0 +1,22 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
+    migrationsRun: false,
+    logging: true,
+    migrationsTableName: "migration",
+    migrations: [__dirname + '/migration/**/*.ts', __dirname + '/migration/**/*.js'],
+    synchronize: false,
+    cli: {
+      migrationsDir: 'src/migration'
+    },
+    autoLoadEntities: true,
+}
+
+export = typeOrmConfig
